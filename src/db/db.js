48dbf18e4fdc3e2e04990898cce1725339577d52
AlexisYazir <20230060@uthh.edu.mx>
2025-03-17 10:17:11 -0600

    Update db.js
    add changue connection db

--- src/db/db.js
+++ src/db/db.js
@@ -1,24 +1,24 @@
 import mongoose from "mongoose";
-// import dotenv from "dotenv";
+import dotenv from "dotenv";
 
-// dotenv.config(); 
+dotenv.config(); 
 
-// export const connectDB = async () => {
-//     try {
-//         await mongoose.connect(process.env.MONGO_URI);
-//         console.log("Conectado a MongoDB Atlas");
-//     } catch (error) {
-//         console.error("Error en la conexión a MongoDB:", error);
-//         process.exit(1);
-//     }
-// };
-
-//PARA CONEXION EN LOCAL SJDJFBOAVMW SDFGB
 export const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb://localhost/merndb");
-        console.log("db success");
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("Conectado a MongoDB Atlas");
     } catch (error) {
-        console.log(error);
+        console.error("Error en la conexión a MongoDB:", error);
+        process.exit(1);
     }
-}
+};
+
+//PARA CONEXION EN LOCAL SJDJFBOAVMW SDFGB
+// export const connectDB = async () => {
+//     try {
+//         await mongoose.connect("mongodb://localhost/merndb");
+//         console.log("db success");
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
