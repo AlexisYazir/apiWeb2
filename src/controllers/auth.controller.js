bce16346855e88ebbb72fa7e8df51b10b3555272
AlexisYazir <20230060@uthh.edu.mx>
2025-03-17 10:36:38 -0600

    Update auth.controller.js
    add token secure

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -83,7 +83,11 @@ export const login = async (req, res) => {
 
     const token = await createAccessToken(tokenPayload);
 
-    res.cookie("token", token);
+    res.cookie("token", token, {
+      httpOnly: true,   // Protege contra accesos desde JavaScript
+      secure: true,     // Solo se enviará en HTTPS
+      sameSite: "None", // Permite que funcione en diferentes dominios
+    });
 
     res.json({
       _id: userFound._id,
