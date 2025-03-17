0ccd789dad2b6cca677d10816dc419f8b7b2372c
AlexisYazir <20230060@uthh.edu.mx>
2025-03-17 11:27:21 -0600

    Update auth.controller.js
    ygo

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -83,8 +83,9 @@ export const login = async (req, res) => {
 
     const token = await createAccessToken(tokenPayload);
 
-  res.cookie("token", token
-            );
+  res.cookie("token", token, {
+  sameSite: "None", // Permite que funcione en diferentes dominios (CORS)
+});
 
     res.json({
       _id: userFound._id,
